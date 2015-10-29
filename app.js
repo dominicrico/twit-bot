f1d831548c446c7c7942975982c983ed50b8d98a
Dominic Rico-Gomez <hello@coderocket.me>
2015-10-29 14:44:07 +0100

    twitter bot to search for most relevant post from igsa and idf

---
+++ app.js
@@ -0,0 +1,56 @@
+var Twit = require('twit'), async = require('async');
+
+var T = new Twit({
+  consumer_key:         process.env.TWIT_CO_KEY,
+  consumer_secret:      process.env.TWIT_CO_SECRET,
+  access_token:         process.env.TWIT_ACC_TOKEN,
+  access_token_secret:  process.env.TWIT_ACC_SECRET
+});
+var tweets = [];
+
+T.get('search/tweets', { q: 'from:@IDFracing OR from:@IGSAworldcup news OR result OR results OR win OR qualifying', result_type: 'recent'}, function(err, data) {
+
+  var tw = data.statuses;
+  for(var i = 0; i<tw.length; i++) {
+    tweets.push({id: tw[i].id, name: tw[i].user.screen_name, date: tw[i].created_at, message: tw[i].text, link: tw[i].entities.urls[0].url, postLink: 'https://twitter.com/' + tw[i].user.screen_name + '/status/' + tw[i].id});
+
+    if (i===tw.length-1) checkTweets();
+  }
+});
+
+function checkTweets() {
+  for (var i=0; i<tweets.length; i++) {
+    tweets[i].prio = 0;
+
+    if (tweets[i].name !== 'IDFRacing') tweets[i].prio += 75;
+
+    if (tweets[i].message.indexOf('race') !== -1 || tweets[i].message.indexOf('quali') !== -1 || tweets[i].message.indexOf('qualifying') !== -1) tweets[i].prio += 35;
+
+    if (tweets[i].message.indexOf('win') !== -1 || tweets[i].message.indexOf('result') !== -1) tweets[i].prio += 40;
+
+    if (tweets[i].message.indexOf('wins') !== -1 || tweets[i].message.indexOf('results') !== -1) tweets[i].prio += 60;
+
+    if (tweets[i].link !== undefined) tweets[i].prio += 20;
+
+    if(i===tweets.length-1) chooseMostRelevantTweet();
+  }
+}
+
+function chooseMostRelevantTweet() {
+  var choosen = null;
+
+  var yesterday = new Date(new Date().getDate() - 1);
+
+  for (var i=0;i<tweets.length;i++) {
+    if (choosen === null || (tweets[i].prio>choosen.prio && new Date(tweets[i].date) > new Date(choosen.date) && new Date(tweets[i].date > yesterday))) choosen = tweets[i];
+
+    if(i===tweets.length-1) postChoosenTweet(choosen);
+  }
+}
+
+function postChoosenTweet(tweet){
+  T.post('statuses/retweet/:id', { id: tweet.id }, function (err, data, response) {
+    console.log(data, response);
+  });
+}
+
